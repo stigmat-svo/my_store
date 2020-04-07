29eef8f97f00e27f2267e4414d93bd27952562d6
stigmat <sheludko.vo@gmail.com>
2020-04-07 12:37:00 +0300

    New edit

---
+++ lib/product_collection.rb
@@ -0,0 +1,46 @@
+class ProductCollection
+  PRODUCT_TYPES = {
+    movie: {dir: "movies", class: Movie},
+    book: {dir: "books", class: Book}
+  }
+
+  def initialize(products = [])
+    @products = products
+  end
+
+  def self.from_dir(dir_path)
+    products = []
+
+    PRODUCT_TYPES.each do |type, hash|
+      product_dir = hash[:dir]
+
+      product_class = hash[:class]
+
+      Dir[dir_path + "/" + product_dir + "/*.txt"].each do |path|
+        products << product_class.from_file(path)
+      end
+    end
+
+    self.new(products)
+  end
+
+  def to_a
+    @products
+  end
+
+  def sort!(params)
+    case params[:by]
+    when :title
+      @products.sort_by! { |product| product.to_s }
+    when :price
+      @products.sort_by! { |product| product.price }
+    when :amount
+      @products.sort_by! { |product| product.amount }
+    end
+
+    @products.reverse! if params[:order] == :asc
+
+    self
+  end
+end
+
--- main.rb
+++ main.rb
@@ -1,15 +1,26 @@
 require_relative "lib/product"
 require_relative "lib/movie"
 require_relative "lib/book"
+require_relative "lib/product_collection"
 
 puts "Список товаров нашего магазина:"
 puts
 
+=begin
 movie = Movie.from_file(File.dirname(__FILE__) + "/data/movies/03.txt")
 book = Book.from_file(File.dirname(__FILE__) + "/data/books/05.txt")
 
 puts movie
 puts book
+=end
+
+collection = ProductCollection.from_dir(File.dirname(__FILE__) + '/data')
+
+collection.sort!(by: :price, order: :asc)
+
+collection.to_a.each do |product|
+  puts product
+end
 
 begin
   Product.from_file(File.dirname(__FILE__) + "/data/movies/*.txt")
