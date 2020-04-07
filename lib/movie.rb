681a68f087919e84735ba42b56e8483328091d8f
stigmat <sheludko.vo@gmail.com>
2020-04-07 10:39:46 +0300

    New edit

--- lib/movie.rb
+++ lib/movie.rb
@@ -1,2 +1,16 @@
 class Movie < Product
+  attr_reader :title, :author, :genre
+
+  def initialize(params)
+    super
+
+    @title = params[:title]
+    @director = params[:author]
+    @year = params[:genre]
+  end
+
+  def to_s
+    "Фильм '#{@title}', #{@year}, режиссер #{@director}, #{super}"
+  end
+
 end
--- main.rb
+++ main.rb
@@ -1,8 +1,46 @@
 require_relative 'lib/product'
-require_relative 'lib/book'
 require_relative 'lib/movie'
+require_relative 'lib/book'
 
-grant = Book.new(price: 150, amount: 2)
+stuff = []
 
-puts "Книга 'В поисках капитана Гранта' стоит #{grant.price} руб. " \
-"Осталось всего #{grant.amount} штуки."
+stuff << Movie.new(
+  title: "От заката до рассвета",
+  genre: "1996",
+  author: "Роберт Родригес",
+  price: 850,
+  amount: 3
+)
+
+stuff << Movie.new(
+  title: "Козырные тузы",
+  genre: "2006",
+  author: "Джо Карнахан",
+  price: 699,
+  amount: 2
+)
+
+stuff << Book.new(
+  title: "Айвенго",
+  genre: "роман",
+  author: "Вальтер Скотт",
+  price: 1250,
+  amount: 15
+)
+
+stuff << Book.new(
+  title: "Ловец орлов",
+  genre: "повесть",
+  author: "Джеймс Уиллард Шульц",
+  price: 1050,
+  amount: 20
+)
+
+puts 'Список товаров нашего магазина:'
+puts
+
+stuff.each do |item|
+  puts item
+end
+
+
